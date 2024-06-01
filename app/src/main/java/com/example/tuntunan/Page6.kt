0a5252a7ab9950571e3bb1963649dcc86c89272c
fernn13_ <fernandaadityo13@gmail.com>
2024-06-01 17:44:39 +0700

    update sholat saat sakit

--- app/src/main/java/com/example/tuntunan/Page6.kt
+++ app/src/main/java/com/example/tuntunan/Page6.kt
@@ -17,7 +17,7 @@ class Page6 : AppCompatActivity() {
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        supportActionBar?.title = "Sholat Jama' dan Qashar"
+        supportActionBar?.title = "Sholat Bagi Orang Sakit"
 
         // Section: Arti
         val artiSakitTitle = findViewById<TextView>(R.id.artiSakitTitle)
